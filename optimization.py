27d163f3baca0ac6642377541c35543ae262f5c8
Saksham Sethi <67079478+sets99@users.noreply.github.com>
2025-02-09 15:40:39 -0500

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -1,58 +1,56 @@
 import numpy as np
-import matplotlib.pyplot as plt 
 
-optimalOrbit = (0, 0, 0)
-a_values = np.linspace(7000e3, 30000e3, 10)
-e_values = np.linspace(0, 0.9, 10)   
-i_values = np.linspace(0, 90, 10)  
+I0 = 1361
+AU = 1.496e11
+MU = 3.986e14
 
-radiation = 0
-minradiation = float('inf')
-min_a = 0
-min_e = 0
-min_i = 0
+def solar_radiation(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    return I0 * (AU / r)**2
 
-RT = 6371000.0  # Earth radius [m]
-m_p = 1.67E-27  # mass of proton [kg]
-m_e = 9.109E-31  # mass of electron [kg]
-qe = 1.602E-19  # charge of proton [C]
-phi = 11.70 * np.pi / 180.0  # Magnetic dipole tilt [rad]
-th = 23.67 * np.pi / 180.0  # Earth’s obliquity [rad]
-mu = -7.94e22 * np.array([0.0, np.sin(phi), np.cos(phi)])  # Earth’s magnetic moment [A m²]
-R0dip = np.array([0.0, 0.0, 0.0])  # Dipole moment location
-M0 = 1.0E-7  # mu0 / 4 pi
-
-# TODO: Update B to use IGRF model
-def B(R, R0, mu):
-    r = np.array([R[0] - R0[0], R[1] - R0[1], R[2] - R0[2]]) * RT
-    rmag = np.sqrt(r[0]**2 + r[1]**2 + r[2]**2)
-    Bfield = M0 * (3.0 * r * np.dot(mu, r) / (rmag**5) - mu / (rmag**3))
-    return Bfield
-
-def radiation(a, e, i):
-    pos = np.linspace(0, 2*np.pi, 1000)
-    r = a*(1-e**2)/(1 + e*np.cos(pos))
-    x = r * np.cos(pos)
-    y = r * np.sin(pos)
+def total_radiation(a, e, i, num_points=1000):
+    t = np.linspace(0, 2 * np.pi, num_points)
+    r = a * (1 - e**2) / (1 + e * np.cos(t))
+    
+    x = r * np.cos(t)
+    y = r * np.sin(t)
     z = r * np.sin(np.radians(i))
 
-    radiation = 0
-    for i in range(1000):
-        radiation += B(x[i], y[i], z[i])
-    
-    return radiation
+    radexposure = 0
+    for j in range(num_points):
+        radiation = solar_radiation(x[j], y[j], z[j])
+        radexposure += radiation
+
+    return radexposure
+
+def compute_perigee_apogee_velocity(a, e):
+    r_p = a * (1 - e)
+    r_a = a * (1 + e)
+    v_p = np.sqrt(MU * (2/r_p - 1/a))
+    v_a = np.sqrt(MU * (2/r_a - 1/a))
+    return (v_p, v_a)
+
+a_min = float(input("Enter minimum value for semi-major axis (a) in meters: "))
+a_max = float(input("Enter maximum value for semi-major axis (a) in meters: "))
+e_min = float(input("Enter minimum value for eccentricity (e): "))
+e_max = float(input("Enter maximum value for eccentricity (e): "))
+i_min = float(input("Enter minimum value for inclination (i) in degrees: "))
+i_max = float(input("Enter maximum value for inclination (i) in degrees: "))
+
+a_values = np.linspace(a_min, a_max, 10)
+e_values = np.linspace(e_min, e_max, 10)
+i_values = np.linspace(i_min, i_max, 5)
+
+min_radiation = float('inf')
+optimalOrbit = None
 
 for a in a_values:
     for e in e_values:
         for i in i_values:
-            if radiation(a, e, i) < minradiation:
-                minradiation = radiation
-                min_a = a
-                min_e = e
-                min_i = i
-
-            else:
-                continue
+            rad = total_radiation(a, e, i)
+            if rad < min_radiation:
+                min_radiation = rad
+                optimalOrbit = (a, e, i)
 
 
-print(min_a + " " + min_e + " " + min_i) 
+print(f"Optimal Orbit: a={optimalOrbit[0]/1e3} km, e={optimalOrbit[1]}, i={optimalOrbit[2]} deg with min radiation {min_radiation:.2e} W/m^2")
